349883f620da9434e99fc8d0bcae723ee093467a
SND\estrizhok_cp <SND\estrizhok_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2011-10-26 14:46:20 +0000

    Assembly information added.
    git-svn-id: https://azonlibrary.svn.codeplex.com/svn@70736 a83551a4-30f6-4d81-a974-c6ced450ddbf

---
+++ Helpers/Properties/AssemblyCommon.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("Azon Helpers Library")]
+[assembly: AssemblyCompany("Eugene Strizhok")]
+[assembly: AssemblyCopyright("Copyright © Eugene Strizhok 2011")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Helpers/Properties/AssemblyInfo.cs
+++ Helpers/Properties/AssemblyInfo.cs
@@ -1,36 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Helpers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Helpers")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("Azon.Helpers")]
+[assembly: AssemblyDescription("A set of fairly small and simple but often required helper classes.")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0362653f-e7f5-4eda-a4e8-f6c6bd67f6cc")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
